94e6d756dae867f66be5ad419b6214ee15af4894
Stanislav Stamatov <stanislav.stamatov45@abv.bg>
2022-06-18 20:35:54 +0300

    Solutions with Iterators and Comparators

---
+++ Iterators and Comparators/BookComparator/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+
+            Book bookTwo = new Book("The Documents in the Case", 2002,
+
+            "Dorothy Sayers", "Robert Eustace");
+
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library libraryOne = new Library();
+
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (Book book in libraryTwo)
+            {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted by the new external Comparer (BookComparator)");
+
+            Book[] books = libraryTwo.ToArray();
+            Array.Sort(books, new BookComparator());
+
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
---
+++ Iterators and Comparators/ComparableBook/Library.cs
@@ -0,0 +1,72 @@
+﻿using IteratorsAndComparators;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            List<Book> bookList = this.books.OrderBy(book => book).ToList();
+            for (int i = 0; i < bookList.Count; i++)
+            {
+                yield return bookList[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        //public IEnumerator<Book> GetEnumerator()
+        //{
+        //    return new LibraryIterator(this.books);
+        //}
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    return GetEnumerator();
+        //}
+
+        //class LibraryIterator : IEnumerator<Book>
+        //{
+        //    private List<Book> books;
+        //    private int position;
+
+        //    public LibraryIterator(List<Book> books)
+        //    {
+        //        this.books = books;
+        //        Reset();
+        //    }
+        //    public Book Current => this.books[position];
+
+        //    object IEnumerator.Current => this.Current;
+
+        //    public void Dispose()
+        //    {
+        //    }
+
+        //    public bool MoveNext()
+        //    {
+        //        this.position++;
+        //        return position < books.Count;
+        //    }
+
+        //    public void Reset()
+        //    {
+        //        this.position = -1;
+        //    }
+        //}
+    }
+}
